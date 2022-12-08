44c67589bfd2f485f28284bffd214aed57547803
Katie Baumgarten <114961083+excitedelephant401@users.noreply.github.com>
2022-12-07 20:09:20 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import "./App.css";
 import "./Navbar.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
-
-
 function App() {
-
-
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
